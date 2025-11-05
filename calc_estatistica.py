380c4022f61f33e6be9d4052ff5fff010a45a425
alexxtananta <alexx.tananta@gmail.com>
2025-11-05 12:25:15 -0400

    desafio-02-resolvido.

--- calc_estatistica.py
+++ calc_estatistica.py
@@ -1,6 +1,6 @@
 from collections import Counter
 
-# 1 Função para calcular a média
+# Função para calcular a média
 
 def calcular_media(lista):
     if not lista:
@@ -10,5 +10,24 @@ def calcular_media(lista):
     media = soma / tamanho
     return media
 
-pass
+# Função para calcular a mediana
 
+def calcular_mediana(lista):
+    lista_ordenada = sorted(lista)
+    tamanho = len(lista_ordenada)
+
+    if tamanho == 0:
+        return 0
+    
+    meio = tamanho // 2
+
+    if tamanho % 2 != 0:
+        mediana = lista_ordenada[meio]
+    else:
+        elemento1 = lista_ordenada[meio - 1]
+        elemento2 = lista_ordenada[meio]
+        mediana = (elemento1 + elemento2) / 2
+        
+    return mediana
+
+pass
